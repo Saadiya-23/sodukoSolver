1ae43235e4746bf049dbc025b19330e82cbc3b58
Saadiya-23 <145015530+Saadiya-23@users.noreply.github.com>
2024-03-31 00:44:15 +0530

    Create Board.java

---
+++ Codes/board/Board.java
@@ -0,0 +1,26 @@
+package board;
+
+public class Board {
+    public int size ;
+    public int matrix[][];
+    public Board(int size) {
+        this.size = size;
+        this.matrix = new int[size][size];
+    }
+
+
+    public void printBoard() {
+        for(int i=0;i<this.size;i++) {
+            for(int j=0;j<this.size;j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void setUpBoardConfig(int row, int col, int val) {
+        this.matrix[row][col] = val;
+    }
+
+    
+}
